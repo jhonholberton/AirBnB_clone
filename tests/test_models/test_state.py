10cb35020f2acaff152204ed3bdbe5149cba1218
Rcastro89 <r_castro_m@yahoo.es>
2022-03-05 16:24:20 -0500

    test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,72 +1,67 @@
 #!/usr/bin/python3
-"""Modulo de pruebas para la clase State"""
+"""
+Test cases for the state class
+"""
+
 from models.base_model import BaseModel
 from models.state import State
 import unittest
-import inspect
-import time
-from datetime import datetime
-from unittest import mock
-import models
 
 
-class TestState(unittest.TestCase):
-    """Probar la clase State"""
+class Teststate(unittest.TestCase):
+    """
+        unitesst for state class
+    """
 
-    def test_sub_class(self):
+    def issub_class(self):
         """
-            probar si la clase de state es una subclase del BaseModel
+            test if state class is sub class of base model
         """
         state = State()
         self.assertIsInstance(state, BaseModel)
         self.assertTrue(hasattr(state, "id"))
         self.assertTrue(hasattr(state, "created_at"))
-        self.assertFalse(hasattr(state, "update_at"))
+        self.assertTrue(hasattr(state, "update_at"))
 
     def test_name(self):
         """
-            Atributo de clase y si esta vacia la cadena
+            test class attribute name
         """
         state = State()
         self.assertTrue(hasattr(state, "name"))
         self.assertEqual(state.name, "")
+        
 
-    def test_to_dictstate_create(self):
+    def test_to_dictstate(self):
         """
-            El método test to_dict crea un diccionario
-            con los atributos adecuados
+            test to dict method with state and the type
+            and content
         """
         state = State()
-        diccionario = state.to_dict()
-        self.assertEqual(type(diccionario), dict)
-        for atributo in state.__dict__:
-            self.assertTrue(atributo in diccionario)
-            self.assertTrue("__class__" in diccionario)
+        dict_cont = state.to_dict()
+        self.assertEqual(type(dict_cont), dict)
+        for attr in state.__dict__:
+            self.assertTrue(attr in dict_cont)
+            self.assertTrue("__class__" in dict_cont)
 
-    def test_str(self):
-        """prueba que el método str tiene la salida correcta"""
+    def test_dict_value(self):
+        """
+            test the returned dictionar values
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         state = State()
-        string = "[State] ({}) {}".format(state.id, state.__dict__)
-        self.assertEqual(string, str(state))
-
-    def test_to_valores_dicci(self):
-        """los valores en dict devueltos desde to_dict sean correctos"""
-        formato = "%Y-%m-%dT%H:%M:%S.%f"
-        state = State()
-        di = state.to_dict()
-        self.assertEqual(di["__class__"], "State")
-        self.assertEqual(type(di["created_at"]), str)
-        self.assertEqual(type(di["updated_at"]), str)
-        self.assertEqual(di["created_at"], state.created_at.strftime(formato))
-        self.assertEqual(di["updated_at"], state.updated_at.strftime(formato))
-
-    def test_instancia(self):
-        """Prueba la instanciación de la clase State"""
-        state = State()
-        self.assertEqual(str(type(state)), "<class 'models.state.State'>")
-        self.assertIsInstance(state, State)
-        self.assertTrue(issubclass(type(state), BaseModel))
+        dict_con = state.to_dict()
+        self.assertEqual(dict_con["__class__"], "State")
+        self.assertEqual(type(dict_con["created_at"]), str)
+        self.assertEqual(type(dict_con["updated_at"]), str)
+        self.assertEqual(
+            dict_con["created_at"],
+            state.created_at.strftime(time_format)
+        )
+        self.assertEqual(
+            dict_con["updated_at"],
+            state.updated_at.strftime(time_format))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
