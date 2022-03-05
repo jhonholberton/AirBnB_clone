e5d9c999d28c7bce6db1d033a26701b833656f66
Rcastro89 <r_castro_m@yahoo.es>
2022-03-05 16:33:18 -0500

    test state

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,33 +1,75 @@
 #!/usr/bin/python3
-"""Unit test for the module City"""
+"""
+Test cases for the city class
+"""
 
-import unittest
+from models.base_model import BaseModel
 from models.city import City
-from models import storage
+import unittest
 
 
-class Test_City(unittest.TestCase):
-    """Test for the class City"""
-    instancia = City()
-    instancia.state_id = 'Betty'
-    instancia.name = 'Bogota'
+class TestCity(unittest.TestCase):
+    """
+        unitesst for city class
+    """
 
-    data_base = storage.all()
-    instancia_nombre = 'City.' + instancia.id
+    def issub_class(self):
+        """
+            test if city class is sub class of base model
+        """
+        city = City()
+        self.assertIsInstance(city, BaseModel)
+        self.assertTrue(hasattr(city, "id"))
+        self.assertTrue(hasattr(city, "created_at"))
+        self.assertTrue(hasattr(city, "update_at"))
 
-    def test_citysave(self):
-        """Test for the method save"""
-        dato_update = self.instancia.updated_at
-        self.instancia.save()
-        new_date = self.instancia.updated_at
-        self.assertNotEqual(dato_update, new_date)
+    def test_name(self):
+        """
+            test class attribute name
+        """
+        city = City()
+        self.assertTrue(hasattr(city, "name"))
+        self.assertEqual(city.name, "")
+    
+    def test_state_id(self):
+        """
+            test class attribute state_id
+        """
+        city = City()
+        self.assertTrue(hasattr(city, "state_id"))
+        self.assertEqual(city.state_id, "")
+        
 
-    def test_citystr(self):
-        """Test for the method __str__"""
-        p = '[City] ({}) {}'.format(self.instancia.id, self.instancia.__dict__)
-        my_string = self.instancia.__str__()
-        self.assertEqual(p, my_string)
+    def test_to_dictcity(self):
+        """
+            test to dict method with city and the type
+            and content
+        """
+        city = City()
+        dict_cont = city.to_dict()
+        self.assertEqual(type(dict_cont), dict)
+        for attr in city.__dict__:
+            self.assertTrue(attr in dict_cont)
+            self.assertTrue("__class__" in dict_cont)
+
+    def test_dict_value(self):
+        """
+            test the returned dictionar values
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        city = City()
+        dict_con = city.to_dict()
+        self.assertEqual(dict_con["__class__"], "City")
+        self.assertEqual(type(dict_con["created_at"]), str)
+        self.assertEqual(type(dict_con["updated_at"]), str)
+        self.assertEqual(
+            dict_con["created_at"],
+            city.created_at.strftime(time_format)
+        )
+        self.assertEqual(
+            dict_con["updated_at"],
+            city.updated_at.strftime(time_format))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
