39db9ca48d90cce295261ebe045a1709fcd38b6b
Rcastro89 <r_castro_m@yahoo.es>
2022-03-05 19:27:35 -0500

    test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,6 +7,13 @@ import os
 
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
@@ -14,24 +21,88 @@ class TestBaseModel(unittest.TestCase):
     """ Testing the FileStorage class of the program.
         """
 
-    def setUp(self):
-        """ Method to prepare each single test.
-            """
-        self.b = FileStorage()
-        self.file = storage._FileStorage__file_path
-        self.b.save()
-        self.storage = FileStorage()
+    instance1 = FileStorage()
+    file = storage._FileStorage__file_path
+    storage = FileStorage()
+    b = FileStorage()
+    b.save()
     
-    def test_new(self):
-        """Test for the method new()"""
-        self.assertIn('new', dir(self.b))
-
     def test_all(self):
         """Test for the method all()"""
-        self.assertIn('all', dir(self.b))
-        self.assertIsInstance(self.b, FileStorage)
-        dictt = self.storage.all()
+        self.assertIn('all', dir(self.instance1))
+        self.assertIsInstance(self.instance1, FileStorage)
+        dictt = storage.all()
         self.assertEqual(type(dictt), dict)
+
+    def test_new(self):
+        """Test for the method new()"""
+        self.assertIn('new', dir(self.instance1))
+
+    def test_save(self):
+        """Test for the method save()"""
+        self.assertIn('save', dir(self.instance1))
+        self.storage.save()
+        self.assertTrue(os.path.isfile(self.file))
+
+    def test_save2(self):
+        '''Test saving a instances of each type'''
+        base = BaseModel()
+        storage.new(base)
+
+        user = User()
+        storage.new(user)
+
+        state = State()
+        storage.new(state)
+
+        place = Place()
+        storage.new(place)
+
+        city = City()
+        storage.new(city)
+
+        amenity = Amenity()
+        storage.new(amenity)
+
+        review = Review()
+        storage.new(review)
+
+        storage.save()
+
+        with open(self.file) as f:
+            string = f.read()
+            self.assertIn("BaseModel." + base.id, string)
+            self.assertIn("User." + user.id, string)
+            self.assertIn("State." + state.id, string)
+            self.assertIn("Place." + place.id, string)
+            self.assertIn("City." + city.id, string)
+            self.assertIn("Amenity." + amenity.id, string)
+            self.assertIn("Review." + review.id, string)
+
+    def test_reload(self):
+        """Test for the method reload()"""
+        self.assertIn('reload', dir(self.instance1))
+
+        objects = storage.all()
+
+        base = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+
+        storage.save()
+        storage.reload()
+
+        self.assertIn("BaseModel." + base.id, objects.keys())
+        self.assertIn("User." + user.id, objects.keys())
+        self.assertIn("State." + state.id, objects.keys())
+        self.assertIn("Place." + place.id, objects.keys())
+        self.assertIn("City." + city.id, objects.keys())
+        self.assertIn("Amenity." + amenity.id, objects.keys())
+        self.assertIn("Review." + review.id, objects.keys())
 
     def test_module_documentation(self):
         """ Test if FileStorage module is documented.
